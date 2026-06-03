bdc234542e5595faf55f36c097af040002de517c
Nekit <nick.misiura@yandex.ru>
2026-06-03 08:34:43 +0300

    Unify global color schemes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
 
     return (
-        <Layout>
+        <Layout className="shadowedText defaultTheme orangeAccent">
             <HomePage />
         </Layout>
     );
