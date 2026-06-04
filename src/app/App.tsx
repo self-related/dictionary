328b4f4ed206d804a187c96a4c3429bda168077d
Nekit <nick.misiura@yandex.ru>
2026-06-04 10:18:21 +0300

    Ref: change app folder structure, remove unnecessary components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import HomePage from "../pages/home/HomePage";
 import Layout from "../shared/ui/layout/Layout";
-import "./ui/styles/global.css";
+import "./styles/global.css";
 
 export default function App() {
 
