dd2cdedae96137bdc2828ce948e712f532b0c011
Nekit <nick.misiura@yandex.ru>
2026-06-04 08:23:21 +0300

    Add checkbox for auto-translation

--- src/features/translate/ui/translate-input/TranslateInput.tsx
+++ src/features/translate/ui/translate-input/TranslateInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./TranslateInput.module.css";
 
 interface TranslateInputProps {
@@ -5,14 +6,27 @@ interface TranslateInputProps {
 }
 
 export default function TranslateInput({ className }: TranslateInputProps) {
+    const [autoTranslate, setAutoTranslate] = useState<boolean>(true);
 
     return (
         <div className={`${styles.inputWrap} shadowedText ${className}`}>
+            {/* input */}
             <textarea
                 className={styles.translateInput}
             />
 
+            {/* checkbox and buttons  */}
             <div className={styles.buttonsWrap}>
+                <label htmlFor="autoTranslate" className={styles.autoTranslateLabel}>
+                    Auto-translation
+                    <input 
+                        id="autoTranslate" 
+                        checked={autoTranslate}
+                        type="checkbox"
+                        onClick={() => setAutoTranslate(value => !value)}
+                    />
+                </label>
+
                 <button>Clear</button>
                 <button>Translate</button>
             </div>
