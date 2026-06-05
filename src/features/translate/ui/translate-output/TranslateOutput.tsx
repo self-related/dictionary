a16d6e1c406c235b7099e6472b0bf5a6eda5502e
Nekit <nick.misiura@yandex.ru>
2026-06-05 07:44:15 +0300

    add reset button

--- src/features/translate/ui/translate-output/TranslateOutput.tsx
+++ src/features/translate/ui/translate-output/TranslateOutput.tsx
@@ -11,7 +11,8 @@ export default function TranslateOutput({ className }: TranslateOutputProps) {
             <textarea value={0} />
 
             <div className={styles.buttonsWrap}>
-                <button>Add -&gt;</button>
+                <button>Reset</button>      {/* reset to default translation (if option was chosen) */}
+                <button>Add -&gt;</button>  {/* add to dictionary */}
             </div>
         </div>
     );
