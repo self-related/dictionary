2d7cc650c354f56658bc73db456aa8942dc3ccbc
Nekit <nick.misiura@yandex.ru>
2026-06-09 07:11:16 +0300

    Fix custom classname attribute for shared ui components

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -9,13 +9,14 @@ type ButtonProps = {
 export default function Button({
     children, 
     transparent = false,
+    className,
     ...rest
 }: ButtonProps) {
     const transparentClass = (transparent) ? styles.transparent : ""; // make transparent if props tell so
     
     return (
         <button 
-            className={`${styles.button} ${transparentClass}`}
+            className={`${styles.button} ${transparentClass} ${className}`}
             {...rest}
         >
             {children}
--- src/shared/ui/select/Select.tsx
+++ src/shared/ui/select/Select.tsx
@@ -2,10 +2,10 @@ import styles from "./Select.module.css";
 
 type SelectProps = React.HTMLAttributes<HTMLSelectElement>;
 
-export default function Select({children, ...rest}: SelectProps) {
+export default function Select({children, className, ...rest}: SelectProps) {
 
     return (
-        <select className={styles.select} {...rest}>
+        <select className={`${styles.select} ${className}`} {...rest}>
             {children}
         </select>
     );
