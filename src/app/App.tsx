5c720a47ccd45102e511134315493d41a2e93b09
Nekit <nick.misiura@yandex.ru>
2026-06-15 07:21:02 +0300

    Configure redux store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
+import { Provider } from "react-redux";
 import HomePage from "../pages/home/HomePage";
 import Layout from "../shared/ui/layout/Layout";
 import "./styles/global.css";
 import "./styles/themes.css";
+import { store } from "./model/store/store";
 
 export default function App() {
 
     return (
-        <Layout>
-            <HomePage />
-        </Layout>
+        <Provider store={store}>
+            <Layout>
+                <HomePage />
+            </Layout>
+        </Provider>
     );
 }
