d286d83c7461f4e2df9c5f592455adc080ec853e
Nekit <nick.misiura@yandex.ru>
2026-07-07 09:07:26 +0300

    Add base url for google

--- src/features/translate/api/providers/google.ts
+++ src/features/translate/api/providers/google.ts
@@ -2,8 +2,7 @@ import type { LangList, LinkGetter } from "../types";
 
 
 export const getGoogleUrl: LinkGetter = ({sourceText, sourceLang, targetLang}) => {
-    // no base url
-    return `translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&dt=bd&dj=1&q=${sourceText}`;
+    return `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&dt=bd&dj=1&q=${sourceText}`;
 };
 
 
