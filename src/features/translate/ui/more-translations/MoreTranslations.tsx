a1dcf33fdbfda1a56ec34e822c4be1aefb4594f7
Nekit <nick.misiura@yandex.ru>
2026-07-13 08:16:41 +0300

    Display more translations from redux store

--- src/features/translate/ui/more-translations/MoreTranslations.tsx
+++ src/features/translate/ui/more-translations/MoreTranslations.tsx
@@ -1,24 +1,17 @@
+import { useAppSelector } from "../../../../app/model/store/hooks";
 import styles from "./MoreTranslations.module.css";
 import Option from "./ui/option/Option";
 
 export default function TranslateOptions() {
+    const translationResult = useAppSelector(state => state.translateSlice.translateResult);
+    const translationElements = translationResult?.moreTranslations?.map(value => <Option key={`t-${value}`} value={value?.translation} />)
+    
     return (
         // Options with more translations
         <div className={styles.translateOptions}>
-            <Option value="test" />
-            <Option value="test" />
-            <Option value="test" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
-            <Option value="testsdasdasdasdsa" />
+            {
+                translationElements
+            }
         </div>
     );
 }
