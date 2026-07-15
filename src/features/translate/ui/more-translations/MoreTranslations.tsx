da36230690388eb01224bb3ebc74a70dd43d70e1
Nekit <nick.misiura@yandex.ru>
2026-07-15 09:22:51 +0300

    Add words groupping for MoreTranslations section

--- src/features/translate/ui/more-translations/MoreTranslations.tsx
+++ src/features/translate/ui/more-translations/MoreTranslations.tsx
@@ -1,18 +1,56 @@
+import type { TranslationVerbose } from "../../api/types";
 import { useSelectTranslateQueryLastResult } from "../../hooks/apiHooks";
 import styles from "./MoreTranslations.module.css";
 import Option from "./ui/option/Option";
 
+interface TranslationsByGroupMap {
+    [group: string]: string[]
+}
 
+// Component
 export default function MoreTranslations() {
     const lastTranslateResult = useSelectTranslateQueryLastResult();
-    const translationElements = lastTranslateResult?.moreTranslations?.map((value, i) => <Option key={`t-${value}-${i}`} value={value?.translation} />);
+    const translationsByGroupMap = groupTranslations(lastTranslateResult?.moreTranslations);
+    const translationsGrouppedJSX = prepGrouppedTranslationsJSX(translationsByGroupMap);
     
     return (
-        // Options with more translations
         <div className={styles.moreTranslations}>
             {
-                translationElements
+                translationsGrouppedJSX
             }
         </div>
     );
 }
+
+
+// Utils
+function groupTranslations(moreTranslations: TranslationVerbose[] = []): TranslationsByGroupMap {
+    const translationsByGroupMap = {} as TranslationsByGroupMap;
+    
+    for (const { wordClass: group, translation } of moreTranslations) {
+        const addedAlready = translationsByGroupMap[group] ?? [];
+        translationsByGroupMap[group] = [translation, ...addedAlready];
+    }
+
+    return translationsByGroupMap;
+}
+
+function prepGrouppedTranslationsJSX(translationsByGroup: TranslationsByGroupMap): React.JSX.Element[] {
+    const groupsJSX = [] as React.JSX.Element[];
+
+    for (const group in translationsByGroup) {
+        const currentGroupWords = translationsByGroup[group]
+        const currentGroupJSX = (
+            <div className={styles.wordGroup} key={`${group}-word-group`}>
+                <span>{group}: </span>
+                {
+                    currentGroupWords.map((value, i) => (<Option key={`t-${i}`} value={value} />))
+                }
+            </div>
+        );
+
+        groupsJSX.push(currentGroupJSX);
+    }
+
+    return groupsJSX;
+};
