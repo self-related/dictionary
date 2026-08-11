6524153622369d3a4b9835c6169cb79d486c20be
Nekit <nick.misiura@yandex.ru>
2026-08-11 07:34:58 +0300

    Add active attribute for shared Button

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -4,6 +4,7 @@ import styles from "./Button.module.css";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     transparent?: boolean,
     grow?: boolean,
+    active?: boolean
 }
 
 export default function Button({
@@ -11,14 +12,16 @@ export default function Button({
     transparent = false,
     grow,
     className = "",
+    active,
     ...rest
 }: ButtonProps) {
     const transparentClass = transparent ? styles.transparent : ""; // make transparent if props tell so
     const growClass = grow ? styles.grow : "";
+    const activeClass = active ? styles.active : "";
 
     return (
         <button 
-            className={`${styles.button} ${transparentClass} ${growClass} ${className}`}
+            className={`${styles.button} ${transparentClass} ${growClass} ${activeClass} ${className}`}
             {...rest}
         >
             {children}
