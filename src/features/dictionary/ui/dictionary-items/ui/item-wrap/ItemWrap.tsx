4b44a194033148e4d683b314dcf619e155bb1daa
Nekit <nick.misiura@yandex.ru>
2026-08-13 08:31:17 +0300

    Prevent blur set on text selection

--- src/features/dictionary/ui/dictionary-items/ui/item-wrap/ItemWrap.tsx
+++ src/features/dictionary/ui/dictionary-items/ui/item-wrap/ItemWrap.tsx
@@ -24,6 +24,7 @@ export default function WordItem({
     const [blurDisabled, setBlurDisabled] = useState(false);
 
     const handleTranslationClick = () => {
+        if (window.getSelection()?.toString()) return; // prevent blur set if text is selected
         setBlurDisabled(val => !val);
     };
 
